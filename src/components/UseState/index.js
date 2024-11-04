2f6f57beb3f3369eca764239af31225eb7f18f2a
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-04 10:30:38 +1000

    used cb functions for updating stated based on previous values

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -4,16 +4,19 @@ import './UseState.css';
 const UseState = () => {
   const [theme, setTheme] = useState('light');
 
+  const [count, setCount] = useState(0);
+
   return (
     <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
       <button onClick={() => setTheme('dark')}>Dark</button>
       <button onClick={() => setTheme('light')}>Light</button>
-      <h2>DISPLAY COUNT HERE</h2>
-      <button>
+      <button onClick={() => setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'))}>Toggle Theme</button>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
         Increment
       </button>
-      <button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>
         Decrement
       </button>
     </div>
